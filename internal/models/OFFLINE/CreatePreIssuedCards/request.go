4002340037703315dcabcb2437f3a63cf4845986
MustafokulovSh <mustafokulovsh@humo.tj>
2026-04-24 15:16:54 +0500

    update commit

--- internal/models/OFFLINE/CreatePreIssuedCards/request.go
+++ internal/models/OFFLINE/CreatePreIssuedCards/request.go
@@ -1,22 +1,35 @@
 package createpreissuedcards
 
 import (
+	models "converterapi/internal/models/OFFLINE"
+	service "converterapi/internal/service/G2B"
 	"converterapi/internal/utils"
+	"converterapi/pkg/logger"
 	"encoding/xml"
 )
 
 // Root - корневой элемент XML
 type Root struct {
-	XMLName xml.Name `xml:"ROOT"`
-	Record  []Record `xml:"RECORD"`
+	XMLName xml.Name         `xml:"ROOT"`
+	Records []models.MRecord `xml:"RECORD"`
 }
 
 func (r Root) GetReqType() string {
 	return string(utils.CreatePreIssuedCards)
 }
 
+func (r Root) GetRecords() []models.MRecord {
+	return r.Records
+}
+func (r Root) GetRecordsCount() int {
+	return len(r.Records)
+}
+
 func (r Root) Call() error {
-	return nil
+	resp, err := service.AddCardG2b(r)
+	logger.Infof("AddCard Mdi call resp: %v", resp)
+	return err
+
 }
 
 // Record - основная запись с данными клиента
