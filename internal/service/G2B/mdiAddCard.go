8e2211c3f605090c776ea1c1956b8641a0ea111d
MustafokulovSh <mustafokulovsh@humo.tj>
2026-04-24 20:01:52 +0500

    lkey alias dismission updates

--- internal/service/G2B/mdiAddCard.go
+++ internal/service/G2B/mdiAddCard.go
@@ -7,10 +7,8 @@ import (
 	"converterapi/internal/utils"
 	"converterapi/pkg/logger"
 	"encoding/json"
-	"fmt"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func AddCardG2b(input models.MDIface) (resp interface{}, err error) {
@@ -19,21 +17,21 @@ func AddCardG2b(input models.MDIface) (resp interface{}, err error) {
 	)
 	recNums := utils.NewSequence()
 
-	filename := fmt.Sprintf("G2BISS-%v.JSON", time.Now().Local().Format("20060102-150405"))
-	header := d8corp.HeaderRecord{
-		IssRectype:      "HEADER",
-		IssRecaction:    "IMPORT",
-		CFilename:       filename, //"G2BISS-20060102-150405.JSON"
-		IssSourcesys:    "LK",
-		IssCompanyRegnr: "ARV",
-		IssTimestamp:    "20230906120000123",
-	}
+	// filename := fmt.Sprintf("G2BISS-%v.JSON", time.Now().Local().Format("20060102-150405"))
+	// header := d8corp.HeaderRecord{
+	// 	IssRectype:      "HEADER",
+	// 	IssRecaction:    "IMPORT",
+	// 	CFilename:       filename, //"G2BISS-20060102-150405.JSON"
+	// 	IssSourcesys:    "LK",
+	// 	IssCompanyRegnr: "ARV",
+	// 	IssTimestamp:    "20230906120000123",
+	// }
 
-	for i, v := range input.GetRecords() {
-		separator := make([]byte, 0)
-		if i != 0 {
-			separator = json.RawMessage(",")
-		}
+	for _, v := range input.GetRecords() {
+		// separator := make([]byte, 0)
+		// if i != 0 {
+		// 	separator = json.RawMessage(",")
+		// }
 
 		var firstSecret, firstName, lastName string
 		if len(v.SecretInfo.Items) != 0 {
@@ -56,7 +54,6 @@ func AddCardG2b(input models.MDIface) (resp interface{}, err error) {
 			DbCdproductCdproduct: "ARVDBT",
 			DbAccountAccnum:      v.Account,
 			DbAccountCurrcode:    v.CurrencyNo,
-			KlLkeyAlias:          "93919",
 			DbCardaCommCat:       "COM03",
 			DbCardaEnroll3ds:     "1",
 			DbCardaLimitCat:      "LIM01",
@@ -71,22 +68,22 @@ func AddCardG2b(input models.MDIface) (resp interface{}, err error) {
 			logger.Errorf("[SERVICE] D8 G2b ADDCARD req marshaling record err: %v", err)
 			return nil, err
 		}
-		recDetails.MdiRecords = append(recDetails.MdiRecords, separator)
+		// recDetails.MdiRecords = append(recDetails.MdiRecords, separator)
 		recDetails.MdiRecords = append(recDetails.MdiRecords, jsonRec)
 	}
 
-	footer := d8corp.FooterRecord{
-		IssRectype:   "FOOTER",
-		IssRecaction: "IMPORT",
-		CFilename:    filename,
-		IssReccnt:    input.GetRecordsCount(),
-	}
+	// footer := d8corp.FooterRecord{
+	// 	IssRectype:   "FOOTER",
+	// 	IssRecaction: "IMPORT",
+	// 	CFilename:    filename,
+	// 	IssReccnt:    input.GetRecordsCount(),
+	// }
 
-	headerJSON, err := json.Marshal(header)
-	if err != nil {
-		logger.Errorf("[SERVICE] D8 G2b ADDCARD req marshaling err: %v", err)
-		return nil, err
-	}
+	// headerJSON, err := json.Marshal(header)
+	// if err != nil {
+	// 	logger.Errorf("[SERVICE] D8 G2b ADDCARD req marshaling err: %v", err)
+	// 	return nil, err
+	// }
 
 	cardJSON, err := json.Marshal(recDetails)
 	if err != nil {
@@ -95,17 +92,17 @@ func AddCardG2b(input models.MDIface) (resp interface{}, err error) {
 	}
 	logger.Infof("json ADDCARD: %v", string(cardJSON))
 
-	footerJSON, err := json.Marshal(footer)
-	if err != nil {
-		logger.Errorf("[SERVICE] D8 G2b ADDCARD req marshaling err: %v", err)
-		return nil, err
-	}
+	// footerJSON, err := json.Marshal(footer)
+	// if err != nil {
+	// 	logger.Errorf("[SERVICE] D8 G2b ADDCARD req marshaling err: %v", err)
+	// 	return nil, err
+	// }
 
 	mdiFile := d8corp.MdiFile{
 		MdiRecords: []json.RawMessage{
-			headerJSON,
+			// headerJSON,
 			cardJSON,
-			footerJSON,
+			// footerJSON,
 		},
 	}
 	mdiDataJSON, err := json.MarshalIndent(mdiFile, "", "  ")
@@ -166,7 +163,6 @@ func AddPreissiedCardG2b(input models.MDIface) (resp interface{}, err error) {
 			DbCdproductCdproduct: "ARVDBT",
 			//DbAccountAccnum:      v.Account,
 			DbAccountCurrcode: v.CurrencyNo,
-			KlLkeyAlias:       "505827",
 			// DbCardaCommCat:    "COM03",
 			DbCardaEnroll3ds: "1",
 			DbCardaLimitCat:  "LIM01",
