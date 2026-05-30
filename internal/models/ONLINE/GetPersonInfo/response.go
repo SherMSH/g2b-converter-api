4fe94bd9112d2ab2a947c26c69a462b4f11f6d45
MustafokulovSh <mustafokulovsh@humo.tj>
2026-05-30 20:03:05 +0500

    GetPersonInfoRq service mock data insertion

--- internal/models/ONLINE/GetPersonInfo/response.go
+++ internal/models/ONLINE/GetPersonInfo/response.go
@@ -25,9 +25,73 @@ type Response struct {
 	TranId       string `xml:"TranId,attr"`
 	Ver          string `xml:"Ver,attr"`
 
-	// Accounts
-	// AlternativeMessaging
-	// Cards
-	// Confidential
-	// Info
+	Accounts             []Account            `xml:"Accounts>Row"`
+	AlternativeMessaging []AlternativeMessage `xml:"AlternativeMessaging>Row"`
+	Cards                []Card               `xml:"Cards>Row"`
+	Confidential         []Confidential       `xml:"Confidential>Row"`
+	Info                 []Info               `xml:"Info>Row"`
+}
+
+// Аккаунт/счет
+type Account struct {
+	PersonId string `xml:"PersonId"`
+	Account  string `xml:"Account"`
+	Type     string `xml:"Type"`
+	Status   string `xml:"Status"`
+}
+
+// Альтернативный канал связи (SMS и т.д.)
+type AlternativeMessage struct {
+	Channel        string `xml:"Channel"`
+	Address        string `xml:"Address"`
+	Scheme         string `xml:"Scheme"`
+	Disabled       string `xml:"Disabled"`
+	UseForDynAuth  string `xml:"UseForDynAuth"`
+	IsDefault      string `xml:"IsDefault"`
+	Broadcast      string `xml:"Broadcast"`
+	DynAuthBlocked string `xml:"DynAuthBlocked"`
+	ErrorCount     string `xml:"ErrorCount"`
+	CreationDate   string `xml:"CreationDate"`
+	LastUpdateTime string `xml:"LastUpdateTime"`
+}
+
+// Банковская карта
+type Card struct {
+	PersonId string `xml:"PersonId"`
+	PAN      string `xml:"PAN"`
+	MBR      string `xml:"MBR"`
+	Status   string `xml:"Status"`
+	ExpDate  string `xml:"ExpDate"`
+	Type     string `xml:"Type"`
+}
+
+// Конфиденциальная информация
+type Confidential struct {
+	PersonId     string `xml:"PersonId"`
+	What         string `xml:"What"`
+	Value        string `xml:"Value"`
+	IsAllowedCST string `xml:"IsAllowedCST"`
+	IsAllowedADS string `xml:"IsAllowedADS"`
+	IsAllowedTB  string `xml:"IsAllowedTB"`
+}
+
+// Персональная информация
+type Info struct {
+	PersonId        string `xml:"PersonId"`
+	FIO             string `xml:"FIO"`
+	Sex             string `xml:"Sex"`
+	IdentType       string `xml:"IdentType"`
+	Identity        string `xml:"Identity"`
+	Birthday        string `xml:"Birthday"`
+	Birthplace      string `xml:"Birthplace"`
+	VIP             string `xml:"VIP"`
+	InstName        string `xml:"InstName"`
+	PersonExtId     string `xml:"PersonExtId"`
+	ResidentCountry string `xml:"ResidentCountry"`
+	AddressInLatin  string `xml:"AddressInLatin"`
+	FirstNameNat    string `xml:"FirstNameNat"`
+	LastNameNat     string `xml:"LastNameNat"`
+	MiddleNameNat   string `xml:"MiddleNameNat"`
+	TaxPayerNumber  string `xml:"TaxPayerNumber"`
+	AddressNat      string `xml:"AddressNat"`
 }
--- internal/models/ONLINE/GetPersonInfo/service.go
+++ internal/models/ONLINE/GetPersonInfo/service.go
@@ -19,5 +19,75 @@ func Svc(b *Body) (soapResp *Envelope, err error) {
 		TranId:       "",
 		Ver:          "1.0",
 	}
+
+	soapResp.Body.GetPersonInfoRp.Response.Accounts = []Account{
+		{
+			PersonId: b.SoapRq.Req.Id,
+			Account:  "20216972600001090524",
+			Type:     "0",
+			Status:   "00 - Active",
+		},
+	}
+
+	soapResp.Body.GetPersonInfoRp.Response.AlternativeMessaging = []AlternativeMessage{
+		{
+			Channel:        "SMSGEN",
+			Address:        "006772212",
+			Scheme:         "ARVD",
+			Disabled:       "0",
+			UseForDynAuth:  "1",
+			IsDefault:      "1",
+			Broadcast:      "1",
+			DynAuthBlocked: "0",
+			ErrorCount:     "0",
+			CreationDate:   "21-04-2026T15:04:05",
+			LastUpdateTime: "21-04-2026T15:04:05",
+		},
+	}
+
+	soapResp.Body.GetPersonInfoRp.Response.Cards = []Card{
+		{
+			PersonId: b.SoapRq.Req.Id,
+			PAN:      "5058270530000032",
+			MBR:      "0",
+			Status:   "00",
+			ExpDate:  "2026-05-31T23:59:59",
+			Type:     "0",
+		},
+	}
+
+	soapResp.Body.GetPersonInfoRp.Response.Confidential = []Confidential{
+		{
+			PersonId:     b.SoapRq.Req.Id,
+			What:         "word",
+			Value:        "confidential_word",
+			IsAllowedCST: "0",
+			IsAllowedADS: "0",
+			IsAllowedTB:  "0",
+		},
+	}
+
+	soapResp.Body.GetPersonInfoRp.Response.Info = []Info{
+		{
+			PersonId:        b.SoapRq.Req.Id,
+			FIO:             "006772212 Давронбек Болтабоев",
+			Sex:             "M",
+			IdentType:       "1",
+			Identity:        "A21404440",
+			Birthday:        "1991-07-19T00:00:00",
+			Birthplace:      "Hudjand",
+			VIP:             "1",
+			InstName:        "ARVD",
+			PersonExtId:     "120147",
+			ResidentCountry: "972",
+			AddressInLatin:  "",
+			FirstNameNat:    "Давронбек",
+			LastNameNat:     "006772212",
+			MiddleNameNat:   "Болтабоев",
+			TaxPayerNumber:  "",
+			AddressNat:      "",
+		},
+	}
+
 	return
 }
